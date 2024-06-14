1f303641d8ff8cf3cfc6f774d0e7b254f69cd50d
Nissi33 <penaortegasamuel@gmail.com>
2024-06-14 18:10:40 -0500

    Case study web documentation finished

---
+++ index.js
@@ -0,0 +1,11 @@
+function toggleMenu() {
+    const menu = document.getElementById('menu');
+    menu.classList.toggle('show-menu');
+}
+
+function toggleSubmenu(id) {
+    const submenu = document.getElementById(id);
+    submenu.classList.toggle('show');
+    
+        
+ }
